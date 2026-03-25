28d8efb09d5da64110d29ff130c9283c72660071
acjmbaghpat <baghpatcourtstaff@gmail.com>
2026-03-25 22:58:10 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,53 +1,32 @@
-const DB_NAME = 'CourtFileManagerDB';
-const DB_VERSION = 1;
-const STORE = 'data';
-
+const DB_NAME="CourtDB";
+const STORE="data";
 let db;
 
-// OPEN DB
 function openDB(){
-  return new Promise((resolve,reject)=>{
-    const req = indexedDB.open(DB_NAME, DB_VERSION);
+  return new Promise((res)=>{
+    const r=indexedDB.open(DB_NAME,1);
 
-    req.onupgradeneeded = (e)=>{
-      db = e.target.result;
-      if(!db.objectStoreNames.contains(STORE)){
-        db.createObjectStore(STORE,{keyPath:'key'});
-      }
+    r.onupgradeneeded=e=>{
+      db=e.target.result;
+      db.createObjectStore(STORE,{keyPath:"key"});
     };
 
-    req.onsuccess = (e)=>{
-      db = e.target.result;
-      resolve(db);
+    r.onsuccess=e=>{
+      db=e.target.result;
+      res();
     };
-
-    req.onerror = (e)=>reject(e);
   });
 }
 
-// SAVE
 function saveData(key,data){
-  return new Promise((resolve)=>{
-    const tx = db.transaction(STORE,'readwrite');
-    tx.objectStore(STORE).put({key,data});
-    tx.oncomplete = resolve;
-  });
+  const tx=db.transaction(STORE,"readwrite");
+  tx.objectStore(STORE).put({key,data});
 }
 
-// LOAD
 function loadData(key){
-  return new Promise((resolve)=>{
-    const tx = db.transaction(STORE,'readonly');
-    const req = tx.objectStore(STORE).get(key);
-
-    req.onsuccess = ()=>{
-      resolve(req.result ? req.result.data : []);
-    };
+  return new Promise(res=>{
+    const tx=db.transaction(STORE,"readonly");
+    const r=tx.objectStore(STORE).get(key);
+    r.onsuccess=()=>res(r.result?.data||[]);
   });
-}
-
-// DELETE
-function deleteData(key){
-  const tx = db.transaction(STORE,'readwrite');
-  tx.objectStore(STORE).delete(key);
 }
